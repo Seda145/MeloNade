7af91ed2ed53b2b00efd3b41cce869d02ec4b210
Seda145 <royict@hotmail.com>
2023-12-08 22:23:39 +0100

    MIT license on parts of code (autocorrolated pitch) of a method on the audioProcessor, borrowed from another library.

--- components/audio-processor.js
+++ components/audio-processor.js
@@ -133,7 +133,30 @@ class AudioProcessor {
         let bUpdatedAutocorrelatedPitch = false;
         if (this.currentRMS > this.minRelevantRMS) {
             // https://github.com/cwilso/PitchDetect/blob/main/js/pitchdetect.js
-            // autocorrelation V2
+            // This method is borrowed from the above library, which has the MIT license.
+            /*
+            The MIT License (MIT)
+
+            Copyright (c) 2014 Chris Wilson
+
+            Permission is hereby granted, free of charge, to any person obtaining a copy
+            of this software and associated documentation files (the "Software"), to deal
+            in the Software without restriction, including without limitation the rights
+            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+            copies of the Software, and to permit persons to whom the Software is
+            furnished to do so, subject to the following conditions:
+
+            The above copyright notice and this permission notice shall be included in all
+            copies or substantial portions of the Software.
+
+            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+            SOFTWARE.
+            */
 
             // Implements the ACF2+ algorithm
             let bufferCopy = this.audioBuffer;
